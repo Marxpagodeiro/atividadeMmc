13e42edb63b0c1c59ba02a3b6c2b48ac4aabfa9e
Marxpagodeiro <113648003+Marxpagodeiro@users.noreply.github.com>
2022-12-04 16:09:01 -0300

    Add files via upload

--- MMC.java
+++ MMC.java
@@ -17,19 +17,46 @@ public class MMC {
         String texto2 = "Digite o numero que sera usado na operação";
         int quant=quant(texto1);
         int[] num = nuns(texto2,quant);
-        int primo = 2;
-        boolean tudo1 = tudo1(num);
-        while (!tudo1(num)){
-            int aux = mmc;
-            System.out.println(print(num) + "|" + primo);
-            divisao(num, primo);
-            if(mmc == aux){
-              primo = proximoPrimo(primo);
+        int primo= 1;
+        int mmc =1;
+        int nDiv = 0;
+        boolean[] primoDiv = new boolean[quant];
+        primoDiv = primoDiv(primo,num,primoDiv);
+        boolean fimloop = false;
+        while (fimloop == false) {
+            boolean printado = false;
+            
+            nDiv = 0;
+            
+            for (int j = 0; j < num.length; j++) {
+                primo = 2;
+                if (num[j] > 1) {
+                    while (num[j] % primo != 0){
+                        primo = proximoPrimo(primo);
+                        primoDiv(primo,num,primoDiv);
+                        if(primoDiv[j]){
+                            print(num, primo);
+                            divisao(num, primo,primoDiv);
+                            mmc = mmc * primo;
+                            printado = true;
+                            break;
+                        }
+                    }
+                }
             }
+            if(printado == false){
+                primoDiv(primo,num,primoDiv);
+                print(num, primo);
+                divisao(num, primo,primoDiv);
+                mmc = mmc * primo;
+            }
+            fimloop = tudo1(num);
+            
         }
-        System.out.println("O resultado final do mmc eh:"+ mmc);
+        System.out.println("O minimo multiplo comum desses numeros é " + mmc);
     }
-    public static int mmc =1;
+    
+    
     public static int[]nuns(String texto1,int quant){
         Scanner in = new Scanner(System.in);
         int[] num = new int[quant]; 
@@ -46,52 +73,63 @@ public class MMC {
         quant=in.nextInt(); 
         return quant;
     }
-    public static String print(int[] vet){
-        String print = "";
-        for(int i = 0; i < vet.length;i++){
-            print += vet[i]+ ",";
+    public static void print(int[] vet,int primo){
+        for(int i = 0; i < vet.length; i++){
+            System.out.print(vet[i] + " ");
         }
-        return print;
+        System.out.println("| " + primo);
+        
     }
     public static boolean tudo1(int[]vet){
-        boolean tudoUm = true;
+        boolean tudoUm = false;
+        int cont = 0;
         for (int i = 0; i < vet.length; i++){
-            if(vet[i] !=1){
-                tudoUm = false;
-                break;
+            if(vet[i] ==1){
+                cont++;
             }
+        }
+        if(cont == vet.length){
+            tudoUm = true;
         }
         return tudoUm;      
     }
-    public static boolean ehPrimo(int primo){
-        boolean primoCerto = true;
-        for(int i = 2; i< (primo/2)+1; i++){
-            if(primo % 2 == 0){
-                primoCerto = false;
-                break;
+    public static int proximoPrimo(int primo){
+        int proxPri = primo + 1;
+        int div = -1;
+        int cont = 1;
+        boolean ehPrimo = true;
+        while (ehPrimo) {
+            for (int j = 0; j < proxPri; j++) {
+                if (proxPri % cont == 0) {
+                    div++;
+                }
+                if (div > 2) {
+                    proxPri++;
+                    break;
+                } else if (div == 2) {
+                    ehPrimo = false;
+                }
+            }
+            cont++;
+        }
+        return proxPri;
+    }
+    public static boolean[] primoDiv(int primo,int []vet,boolean[]primoDiv){
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] % primo == 0 && vet[i] > 1) {
+                primoDiv[i] = true;
+            } else {
+                primoDiv[i] = false;
             }
         }
-        return primoCerto;
+        return primoDiv;
+       
     }
-    public static int proximoPrimo(int primo){
-        int proxPri = primo;
-        do{
-            proxPri=primo+1;
-        }while (!ehPrimo(proxPri));
-        return proxPri;
-      
-    }
-    public static void divisao(int[] vet,int primo){
-        int aux = 0;
-        for(int i = 0; i < vet.length; i++){
-           if(vet[i] % primo == 0){
-               vet[i] = vet[i] / primo;
-               aux +=1;
-           }
+    public static void divisao(int[] vet,int primo,boolean[] primoDiv){
+        for (int i = 0; i < vet.length; i++) {
+            if (primoDiv[i] == true) {
+                vet[i] = vet[i] / primo;
+            }
         }
-        if(aux > 0){
-            mmc *= primo;
-        }    
     }
-        
 }
